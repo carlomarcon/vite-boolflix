80bdfcad0021725b2635e2bb1d901e53796f9471
carlomarcon <carlomarcon1998@gmail.com>
2023-11-29 13:51:57 +0100

    ADD: COMPONENTS

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
-<script setup>
+<script>
 import axios from "axios";
 import { store } from "./components/store";
+import appHeader from "./components/appHeader.vue";
+import appBody from "./components/appBody.vue";
+import appFooter from "./components/appFooter.vue";
+
+export default {
+  components: {
+    appHeader,
+    appBody,
+    appFooter,
+  },
+};
 </script>
 
 <template></template>
